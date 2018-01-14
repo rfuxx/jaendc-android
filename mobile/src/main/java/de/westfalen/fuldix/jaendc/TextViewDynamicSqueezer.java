92e8e0ce647d016b4dfe000e34c4e92a17d0a59e
Roland Fulde <rfuxx@users.noreply.github.com>
2018-01-14 20:59:40 +0100

    text squeezer reworked

--- mobile/src/main/java/de/westfalen/fuldix/jaendc/TextViewDynamicSqueezer.java
+++ mobile/src/main/java/de/westfalen/fuldix/jaendc/TextViewDynamicSqueezer.java
@@ -1,53 +1,90 @@
 package de.westfalen.fuldix.jaendc;
 
-import android.annotation.TargetApi;
 import android.app.Activity;
+import android.os.Build;
 import android.text.Layout;
 import android.text.TextPaint;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewParent;
 import android.view.ViewTreeObserver;
 import android.widget.TextView;
+import android.widget.Toolbar;
 
 public class TextViewDynamicSqueezer {
     private static class AdjustTextScalexListener implements ViewTreeObserver.OnGlobalLayoutListener {
+        private static final int MAX_ITERATIONS = 10;
         private TextView textView;
         private CharSequence content;
         private float adjustedForWidth;
         private float adjustedForHeight;
+        private float adjustedTextScaleX = 1;
         private float origTextScaleX;
 
-        @TargetApi(11)
+        private static boolean isViewInToolbar(final View checkView) {
+            if (Build.VERSION.SDK_INT >= 21) {
+                if (checkView instanceof Toolbar) {
+                    return true;
+                } else if (checkView != null) {
+                    final ViewParent parent = checkView.getParent();
+                    return parent instanceof View && isViewInToolbar((View) parent);
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+
         @Override
         public void onGlobalLayout () {
             final Layout l = textView.getLayout();
             if (l != null) {
-                final float allowedWidth = textView.getWidth() - textView.getTotalPaddingLeft() - textView.getTotalPaddingRight();
-                final float allowedHeight = textView.getHeight() - textView.getTotalPaddingTop() - textView.getTotalPaddingBottom();
+                final ViewParent parent = textView.getParent();
+                final float allowedWidth;
+                final float allowedHeight;
+                if(parent instanceof View && !isViewInToolbar((View) parent)) {
+                    final View parentView = (View) parent;
+                    allowedWidth = parentView.getWidth() - parentView.getPaddingLeft() - parentView.getPaddingRight() - textView.getTotalPaddingLeft() - textView.getTotalPaddingRight();
+                    allowedHeight = parentView.getHeight() - parentView.getPaddingTop() - parentView.getPaddingBottom() - textView.getTotalPaddingTop() - textView.getTotalPaddingBottom();
+                } else {
+                    allowedWidth = textView.getWidth() - textView.getTotalPaddingLeft() - textView.getTotalPaddingRight();
+                    allowedHeight = textView.getHeight() - textView.getTotalPaddingTop() - textView.getTotalPaddingBottom();
+                }
                 final CharSequence currentContent = textView.getText();
-                if (adjustedForHeight != allowedHeight || adjustedForWidth != allowedWidth || !currentContent.equals(content)) {
+                //System.out.println("check for :"+currentContent + "| currentTextScaleX: " + textView.getTextScaleX());
+                if (allowedWidth > 0 && (adjustedForHeight != allowedHeight || adjustedForWidth != allowedWidth || !currentContent.equals(content))) {
                     final TextPaint textPaint = textView.getPaint();
-                    textPaint.setTextScaleX(origTextScaleX);
-                    final float desiredWidth = Layout.getDesiredWidth(currentContent, textPaint);
-// System.out.println("#####"+currentContent + "|  " + allowedWidth+"/"+desiredWidth + " and height " + allowedHeight+"/");
-                    final float scaleX = allowedWidth < desiredWidth ? allowedWidth / desiredWidth : origTextScaleX;
-                    if(scaleX != textView.getTextScaleX()) {
-                        textView.setTextScaleX(scaleX);
+                    float scaleX = origTextScaleX;
+                    for(int i=0; i< MAX_ITERATIONS; i++) {
+                        textPaint.setTextScaleX(scaleX);
+                        final float desiredWidth = Layout.getDesiredWidth(currentContent, textPaint);
+                        if(allowedWidth < desiredWidth) {
+                            final float previousScaleX = scaleX;
+                            scaleX = allowedWidth / desiredWidth * scaleX;
+                            if(scaleX > 1 / desiredWidth && desiredWidth * (previousScaleX- scaleX) < 1) {  // if scale change was less than a pixel
+                                //System.out.println("adjust " + scaleX + " because " + desiredWidth * (previousScaleX- scaleX));
+                                scaleX -= (1 / desiredWidth);   // ensure the change is one pixel
+                            }
+                            //System.out.println("content:"+currentContent + "| scale:" + scaleX + ", width " + allowedWidth+"/"+desiredWidth + " and height " + allowedHeight+"/");
+                        } else {
+                            //System.out.println("finally content:"+currentContent + "| scale:" + scaleX + ", width " + allowedWidth+"/"+desiredWidth + " and height " + allowedHeight+"/");
+                            break;
+                        }
                     }
                     content = currentContent;
                     adjustedForWidth = allowedWidth;
                     adjustedForHeight = allowedHeight;
+                    adjustedTextScaleX = scaleX;
+                }
+                if(adjustedTextScaleX != textView.getTextScaleX()) {
+                    textView.setTextScaleX(adjustedTextScaleX);
                 }
             }
         }
 
         public void setTextView(final TextView textView) {
             this.textView = textView;
-            final CharSequence text = textView.getText();
-            if(text != null && text.length() > 0 && text.charAt(text.length()-1) != '\u00a0') {
-                final CharSequence textWithEnd = text + "\u00a0";   // append protected space so that there is enough "visual buffer"
-                textView.setText(textWithEnd);
-            }
             origTextScaleX = textView.getTextScaleX();
             textView.setEllipsize(null);
         }
